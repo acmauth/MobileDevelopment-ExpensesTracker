d82f648bcd6bfb22405d525f119bbe251c77f527
constantinosloizou <kvloizou@csd.auth.gr>
2021-03-24 16:54:53 +0200

    Added Java activity files : TotalSpentActivity.java TransactionActivity.java TransactionCategotiesActivity.java
    TotalSpentActivity.java : Dynamic title set
    
    TransactionActivity.java : Dynamic title set through Bundle
    
    TransactionCategotiesActivity.java : Dynamic title set, dynamic buttons creation and onClick listeners
    
    categories_activity.xml file renamed to transaction_categories_activity.xml
    
    categories.xml file deleted (No need to keep it)
    
    Added "add money" option in transaction_categories_activity.xml
    
    Added category_button.xml f(or dynamic creation of buttons in java)
    
    total_spent_activity.xml changed from LinearLayout to ConstrainLayout
    
    New app colors
    
    New strings
    
    New font and text sizes
    
    Changes on previous pull requests
    
    Default phone navigator disabled

--- ExpensesTracker/app/src/main/java/com/example/expensestracker/MainActivity.java
+++ ExpensesTracker/app/src/main/java/com/example/expensestracker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_activity);
-    }
 
-    public void foo (){
-        return;
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
--- ExpensesTracker/app/src/main/java/com/example/expensestracker/TransactionActivity.java
+++ ExpensesTracker/app/src/main/java/com/example/expensestracker/TransactionActivity.java
@@ -8,16 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class TotalSpentActivity extends AppCompatActivity {
+public class TransactionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.total_spent_activity);
-
-        TextView title = findViewById(R.id.title_of_top_bar); // gets TextView (top bar title)
-        title.setText(R.string.money_spent_each_category_title); // sets title for top bar
+        setContentView(R.layout.transaction_activity);
 
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -26,6 +24,14 @@ public class TotalSpentActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        Bundle b = getIntent().getExtras(); // gets bundle from Intent
+        TextView textView = findViewById(R.id.title_of_top_bar); // gets TextView (top bar title)
+
+        if(b != null){
+            String categoryName = b.getString("category"); // gets category name
+            textView.setText(categoryName); // sets title for top bar
+        }
 
     }
 
@@ -49,4 +55,5 @@ public class TotalSpentActivity extends AppCompatActivity {
         }
 
     }
+
 }
