de51e04522e70b648d3136c4b461cbaf6d2ef145
constantinosloizou <kvloizou@csd.auth.gr>
2021-04-01 22:38:03 +0300

    initial activity back end

---
+++ ExpensesTracker/app/src/main/java/com/example/expensestracker/InitialActivity.java
@@ -0,0 +1,107 @@
+package com.example.expensestracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InitialActivity extends AppCompatActivity {
+
+    private Button submitButton;
+    private String name;
+    private double budget;
+    private String periodSelection;
+    private boolean options = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_activity);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                if (allFieldsFilled()){
+
+                }
+
+            }
+        });
+    }
+
+    public boolean setName(){
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        name = nameTextView.getText().toString();
+        return true;
+    }
+
+    public boolean setBudget(){
+
+        String budget;
+        TextView budgetTextView = findViewById(R.id.budgetTextView);
+        budget = budgetTextView.getText().toString();
+
+        try {
+            this.budget = Double.parseDouble(budget);
+        }
+        catch (NumberFormatException nfe){
+            budgetTextView.setHint("*Required Field");
+            budgetTextView.setHintTextColor(Color.RED);
+            budgetTextView.setText("");
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public boolean setPeriodSelection(){
+
+        RadioButton monthRadioButton = findViewById(R.id.monthRadioButton);
+        RadioButton weekRadioButton = findViewById(R.id.weekRadioButton);
+
+        if (monthRadioButton.isChecked()){
+            periodSelection = "month";
+        }
+        else if (weekRadioButton.isChecked()){
+            periodSelection = "week";
+        }
+        else {
+            TextView textView = findViewById(R.id.warnChoices);
+            textView.setVisibility(View.VISIBLE);
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean allFieldsFilled(){
+        if (!setName()){
+            return false;
+        }
+        if (!setBudget()){
+            return false;
+        }
+        if (!setPeriodSelection()){
+            return false;
+        }
+        return true;
+    }
+
+
+    public void openCategoriesActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
